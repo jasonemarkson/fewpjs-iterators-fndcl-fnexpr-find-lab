155e5acb3970b905ce655b5863884fd77878499c
jasonemarkson <jasonmarkson76@gmail.com>
2021-06-09 21:57:56 -0400

    commit tree 301897b9db313e2581f5c5960ab047afdca82583

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function superbowlWin(array) {
+    const win = array.find(superBowl => superBowl.result === "W")
 
+    if (win) {
+        return win.year
+    } else {
+        return undefined
+    }
+}
